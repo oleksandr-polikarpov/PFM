31d1edae22e9593e7e2ca446713e993c28e66a1f
oleksandr-polikarpov <86711667+oleksandr-polikarpov@users.noreply.github.com>
2021-11-09 16:35:10 +0200

    Update Docker_inst.sh

--- Docker_inst.sh
+++ Docker_inst.sh
@@ -16,9 +16,24 @@ sudo mkfs.xfs /dev/sdc1
 sudo partprobe /dev/sdc1
 
 sudo mkdir /disk
+sudo mkdir /mnt/shared
 
 sudo mount /dev/sdc1 /disk
 
 uuid=$(sudo blkid /dev/sdc1 -o value | head -n 1)
 sudo bash -c 'echo "UUID=$0 /disk   xfs   defaults,nofail   1   2" >> /etc/fstab' $uuid
 sudo chgrp -R docker /disk
+
+echo $1 > diskKey.key
+
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/corraibidatastorage.cred" ]; then
+    sudo bash -c 'echo "username=corraibidatastorage" >> /etc/smbcredentials/corraibidatastorage.cred'
+    sudo bash -c 'echo "password=$0 >> /etc/smbcredentials/corraibidatastorage.cred' $1
+fi
+sudo chmod 600 /etc/smbcredentials/corraibidatastorage.cred
+sudo bash -c 'echo "//corraibidatastorage.file.core.windows.net/corraibifilestorage /mnt/shared cifs nofail,vers=3.0,credentials=/etc/smbcredentials/corraibidatastorage.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+sudo mount -t cifs //corraibidatastorage.file.core.windows.net/corraibifilestorage /mnt/shared -o vers=3.0,credentials=/etc/smbcredentials/corraibidatastorage.cred,dir_mode=0777,file_mode=0777,serverino
+
